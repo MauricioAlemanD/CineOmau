485613b92e9e8f513e51a7b273ead504bdae9863
Mauricio Leonardo Alemán Díaz <mauricio.leonardo.aleman.diaz1@gmail.com>
2023-10-14 22:45:06 -0600

    Cambie etiqueta

--- CineOmau/CineOmau/Form1.Designer.vb
+++ CineOmau/CineOmau/Form1.Designer.vb
@@ -28,9 +28,10 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(341, 32)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(183, 32)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.Size = New System.Drawing.Size(385, 73)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "CINEOMAU"
         '
