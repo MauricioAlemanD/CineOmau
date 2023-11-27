ea92be71c439df1f9d953972bfef2621e0031b31
Mauricio Leonardo Alemán Díaz <mauricio.leonardo.aleman.diaz1@gmail.com>
2023-11-27 09:30:10 -0600

    aa

--- CineOmau/CineOmau/Form1.Designer.vb
+++ CineOmau/CineOmau/Form1.Designer.vb
@@ -30,8 +30,6 @@ Partial Class Form1
         Me.btnIngresar = New System.Windows.Forms.Button()
         Me.btnRegistrar = New System.Windows.Forms.Button()
         Me.btnSalir = New System.Windows.Forms.Button()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -40,10 +38,9 @@ Partial Class Form1
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.DarkRed
-        Me.Label1.Location = New System.Drawing.Point(244, 11)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(183, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(478, 91)
+        Me.Label1.Size = New System.Drawing.Size(385, 73)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "CINEOMAU"
         '
@@ -51,10 +48,9 @@ Partial Class Form1
         '
         Me.Label2.Font = New System.Drawing.Font("Gadugi", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.Silver
-        Me.Label2.Location = New System.Drawing.Point(253, 144)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(190, 117)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(347, 43)
+        Me.Label2.Size = New System.Drawing.Size(260, 35)
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "Correo Electronico:"
         '
@@ -62,20 +58,18 @@ Partial Class Form1
         '
         Me.txtCorreo.BackColor = System.Drawing.Color.DimGray
         Me.txtCorreo.Font = New System.Drawing.Font("Gadugi", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCorreo.Location = New System.Drawing.Point(261, 191)
-        Me.txtCorreo.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtCorreo.Location = New System.Drawing.Point(196, 155)
         Me.txtCorreo.Name = "txtCorreo"
-        Me.txtCorreo.Size = New System.Drawing.Size(495, 42)
+        Me.txtCorreo.Size = New System.Drawing.Size(372, 35)
         Me.txtCorreo.TabIndex = 1
         '
         'Label3
         '
         Me.Label3.Font = New System.Drawing.Font("Gadugi", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.Silver
-        Me.Label3.Location = New System.Drawing.Point(253, 252)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(190, 205)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(212, 43)
+        Me.Label3.Size = New System.Drawing.Size(159, 35)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Contraseña:"
         '
@@ -83,10 +77,9 @@ Partial Class Form1
         '
         Me.txtContraseña.BackColor = System.Drawing.Color.DimGray
         Me.txtContraseña.Font = New System.Drawing.Font("Gadugi", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtContraseña.Location = New System.Drawing.Point(261, 299)
-        Me.txtContraseña.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtContraseña.Location = New System.Drawing.Point(196, 243)
         Me.txtContraseña.Name = "txtContraseña"
-        Me.txtContraseña.Size = New System.Drawing.Size(495, 42)
+        Me.txtContraseña.Size = New System.Drawing.Size(372, 35)
         Me.txtContraseña.TabIndex = 3
         '
         'btnIngresar
@@ -94,10 +87,9 @@ Partial Class Form1
         Me.btnIngresar.BackColor = System.Drawing.Color.DarkRed
         Me.btnIngresar.Font = New System.Drawing.Font("Gadugi", 20.25!)
         Me.btnIngresar.ForeColor = System.Drawing.Color.Silver
-        Me.btnIngresar.Location = New System.Drawing.Point(261, 372)
-        Me.btnIngresar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnIngresar.Location = New System.Drawing.Point(196, 302)
         Me.btnIngresar.Name = "btnIngresar"
-        Me.btnIngresar.Size = New System.Drawing.Size(496, 47)
+        Me.btnIngresar.Size = New System.Drawing.Size(372, 38)
         Me.btnIngresar.TabIndex = 4
         Me.btnIngresar.Text = "INGRESAR"
         Me.btnIngresar.UseVisualStyleBackColor = False
@@ -107,10 +99,9 @@ Partial Class Form1
         Me.btnRegistrar.BackColor = System.Drawing.Color.DarkRed
         Me.btnRegistrar.Font = New System.Drawing.Font("Gadugi", 20.25!)
         Me.btnRegistrar.ForeColor = System.Drawing.Color.Silver
-        Me.btnRegistrar.Location = New System.Drawing.Point(261, 450)
-        Me.btnRegistrar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnRegistrar.Location = New System.Drawing.Point(196, 366)
         Me.btnRegistrar.Name = "btnRegistrar"
-        Me.btnRegistrar.Size = New System.Drawing.Size(496, 47)
+        Me.btnRegistrar.Size = New System.Drawing.Size(372, 38)
         Me.btnRegistrar.TabIndex = 5
         Me.btnRegistrar.Text = "REGISTRAR"
         Me.btnRegistrar.UseVisualStyleBackColor = False
@@ -120,31 +111,19 @@ Partial Class Form1
         Me.btnSalir.BackColor = System.Drawing.Color.DarkRed
         Me.btnSalir.Font = New System.Drawing.Font("Gadugi", 20.25!)
         Me.btnSalir.ForeColor = System.Drawing.Color.Silver
-        Me.btnSalir.Location = New System.Drawing.Point(921, 492)
-        Me.btnSalir.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnSalir.Location = New System.Drawing.Point(691, 400)
         Me.btnSalir.Name = "btnSalir"
-        Me.btnSalir.Size = New System.Drawing.Size(129, 47)
+        Me.btnSalir.Size = New System.Drawing.Size(97, 38)
         Me.btnSalir.TabIndex = 6
         Me.btnSalir.Text = "SALIR"
         Me.btnSalir.UseVisualStyleBackColor = False
         '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.CineOmau.My.Resources.Resources._removal_ai__5a28fc1c_d3b5_450e_a01a_1c428e0854c1_oig_wfpynuw6fz
-        Me.PictureBox1.Location = New System.Drawing.Point(3, 2)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(164, 163)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox1.TabIndex = 31
-        Me.PictureBox1.TabStop = False
-        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(1067, 554)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.btnSalir)
         Me.Controls.Add(Me.btnRegistrar)
         Me.Controls.Add(Me.btnIngresar)
@@ -153,10 +132,8 @@ Partial Class Form1
         Me.Controls.Add(Me.txtCorreo)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -170,5 +147,4 @@ Partial Class Form1
     Friend WithEvents btnIngresar As Button
     Friend WithEvents btnRegistrar As Button
     Friend WithEvents btnSalir As Button
-    Friend WithEvents PictureBox1 As PictureBox
 End Class
